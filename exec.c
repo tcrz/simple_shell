ea38b023698c1f612debe974033f65f69f311c5c
f-i-t-s-u-m <fibabirr@gmail.com>
2021-10-24 20:20:26 +0300

    update exec.c

---
+++ exec.c
@@ -0,0 +1,12 @@
+#include "header.h"
+/**
+ *execute_cmd - run a command 
+ *@cmd: the command extracted from shell.c
+ *Return: 1 if problem with execution or 0 if success
+ *
+ */
+int execute_cmd(char *cmd)
+{
+	char *params[20];
+
+}
