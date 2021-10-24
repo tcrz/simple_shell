0411f5000a0a4c4e496ca76d7cca5b4689b3ade9
f-i-t-s-u-m <fibabirr@gmail.com>
2021-10-24 10:42:05 +0300

    update header.h

--- header.h
+++ header.h
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+int _putchar(char str);
 void _print(char *str);
 int _strlen(char *str);
+int _strcmp(char *str, char *strcmp);
 #endif
