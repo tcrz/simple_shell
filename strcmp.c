f472e9692e72dd2cd4e630115ed984f761d8a219
Tonycrz <tonybaidoo0@gmail.com>
2021-10-30 18:40:46 +0000

    fixed function

--- strcmp.c
+++ strcmp.c
@@ -1,15 +1,26 @@
 #include "header.h"
 
+/**
+* _strcmp - compares two strings
+* @s1: first string
+* @s2: second string
+* Return: int value
+*/
 
-int _strcmp(char *str, char *strcom)
+
+
+int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+        while ((*s1 && *s2 != '\0') && *s1 == *s2)
+        {
+                s1++;
+                s2++;
+        }
 
-	while (str[i] != '\0' && strcom[i] != '\0')
-	{
-		if (str[i] != strcom[i])
-			return (1);
-		i++;
-	}
-	return (0);
+        if (*s1 == *s2)
+                return (0);
+        else
+                return (*s1 - *s2);
 }
+
+
