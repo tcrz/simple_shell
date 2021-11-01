b834f9f74c5c6060ce8e6b73958ef9a50d67e0e0
Tonycrz <tonybaidoo0@gmail.com>
2021-11-01 22:46:02 +0000

    added builtin commands

--- exec.c
+++ exec.c
@@ -7,28 +7,38 @@
  *
  */
 
-void execute(char **args)
+int execute(char **args)
 {
-	int id;
-	int status;
+	int i, id, status;
+
+	 if (!args[0])
+		args[0] = "";
+	for (i = 0; i < num_of_builtins(); i++)
+	{
+		if (_strcmp(args[0], builtins_cmd[i]) == 0)
+			return ((*builtins_func[i])(args));
+	}
 
 	if (access(_strcat(args[0]), X_OK))
 	{
-		perror("error!");
-		return;
+		_print(args[0]);
+		_print(": command not found\n");
+		return (1);
 	}
 	id = fork();
 	if (id < 0)
 	{
-		perror("Fork failed");
+		_print("Fork failed");
 		exit(EXIT_FAILURE);
 	}
 	if (id == 0)
 	{
 		if (execve(_strcat(args[0]), args, NULL) == -1)
-			perror("error!");
+			_print("Error! Execution failed");
 		exit(EXIT_FAILURE);
 	}
 	else
 		wait(&status);
+
+	return (1);
 }
