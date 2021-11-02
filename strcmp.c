3e8e652d82664af360a53ec451919be92827f331
Tonycrz <tonybaidoo0@gmail.com>
2021-11-02 10:03:00 +0000

    added strncmp.c

--- strcmp.c
+++ strcmp.c
@@ -20,3 +20,28 @@ int _strcmp(char *s1, char *s2)
 	else
 		return (*s1 - *s2);
 }
+
+
+/**
+* _strncmp - compares two strings
+* @s1: first string
+* @s2: second string
+* @n: number of bytes to compare
+* Return: int value
+*/
+
+
+int _strncmp(char *s1, char *s2, size_t n)
+{
+
+        if (n == 0)
+                return (0);
+        do {
+                if (*s1 != *s2++)
+                        return (*s1 - *s2 - 1);
+                if (*s1++ == 0)
+                        break;
+        } while (--n != 0);
+        return (0);
+
+}
