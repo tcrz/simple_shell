a6b97ba5355ecce3570f55bfa71c30121c12bac8
Tonycrz <tonybaidoo0@gmail.com>
2021-11-03 14:04:00 +0000

    main file

---
+++ shell.c
@@ -0,0 +1,74 @@
+#include "header.h"
+
+/**
+ * split_str - splits a string into tokens
+ * @buf: The character to print
+ * Return: array of strings.
+ */
+char **split_str(char *buf)
+{
+	char *token, **eachstr, *delim;
+	int count = 0;
+
+	delim = " \t\r\n\a";
+	eachstr = malloc(sizeof(*eachstr) * 100);
+	if (!eachstr)
+		return (NULL);
+
+	token = strtok(buf, delim);
+	while (token)
+	{
+		eachstr[count] = token;
+		token = strtok(NULL, delim);
+
+		count++;
+	}
+	eachstr[count] = NULL;
+
+	return (eachstr);
+}
+
+/**
+ * main - the main program for shell.
+ * @ac: Arguments numbers.
+ * @av: string arguments.
+ * Return: 0 always.
+ */
+int main(int ac, char **av)
+{
+	unsigned int status = 1;
+	char *userinput, **args;
+	size_t userinput_size = 0;
+	int line, counter = 1;
+
+	signal(SIGINT, SIG_IGN);
+	ac = ac;
+	userinput = malloc(userinput_size);
+	if (!userinput)
+	{
+		_print("Failed, can not allocate memory!");
+		exit(EXIT_FAILURE);
+	}
+
+	while (status)
+	{
+		if (isatty(STDIN_FILENO))
+			_print("($) ");
+		line = getline(&userinput, &userinput_size, stdin);
+		if (line == -1)
+		{
+			status = 0;
+			if (isatty(STDIN_FILENO))
+				_putchar('\n');
+			exit(EXIT_SUCCESS);
+		}
+		args = split_str(userinput);
+		status = execute(av[0], args, counter);
+
+		counter++;
+		free(args);
+	}
+	free(userinput);
+
+	return (0);
+}
