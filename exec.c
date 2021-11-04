8b6799a6e300c7b9d4e75a1dacb75971b40de569
Tonycrz <tonybaidoo0@gmail.com>
2021-11-04 10:57:03 +0000

    betty check

--- exec.c
+++ exec.c
@@ -49,21 +49,23 @@ void errormsg(char *filename, char *args, int count)
 int execute(char *filename, char **args, int count)
 {
 	int i, id, status;
-	/*char *firstarg = args[0]; */
+	/*
+	*char *firstarg = args[0];
+	*/
 	char *builtins_cmd[] = {"env", "exit", "cd", "help"};
 	int builtins_num = sizeof(builtins_cmd) / sizeof(char *);
 
 	if (!args[0])
 		return (1);
-
 	for (i = 0; i < builtins_num; i++)
 	{
 		if (_strcmp(args[0], builtins_cmd[i]) == 0)
 			return ((*builtins_func[i])(args));
 	}
-	
-	/*if (_strncmp(firstarg, "/", 1) != 0)
-		firstarg = _strcat(args[0]); */
+	/*
+	*	if (_strncmp(firstarg, "/", 1) != 0)
+	*	firstarg = _strcat(args[0]);
+	*/
 	if (access(args[0], X_OK))
 	{
 		errormsg(filename, args[0], count);
@@ -78,14 +80,12 @@ int execute(char *filename, char **args, int count)
 	if (id == 0)
 	{
 		if (execve(args[0], args, NULL) == -1)
-		{	
+		{
 			perror("error");
-			
 		}
 		exit(EXIT_FAILURE);
 	}
 	else
 		wait(&status);
-
 	return (1);
 }
